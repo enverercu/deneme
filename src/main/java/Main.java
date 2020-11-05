b3dcaa647177e7bc0ac407f8e2cc41fb7ee567ac
Enver Ercu Yilmaz <enyilmaz@lmka-2dk0dzml85.united.domain>
2020-11-05 14:14:05 +0100

    third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("hello world");
-        System.out.println(findSum(1,66));
+        System.out.println("The sum is : " + findSum(1,66));
     }
 
     public static int findSum(int x, int y){
